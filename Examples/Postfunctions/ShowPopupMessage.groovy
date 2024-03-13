16050019ec414d10ca8dd281db88c7f7ad35c519
Evgeniy Isaenkov <udjin79@gmail.com>
2024-03-13 22:13:16 +0300

    feat(Added example of showing popup message for operations, that run in background):

---
+++ Examples/Postfunctions/ShowPopupMessage.groovy
@@ -0,0 +1,13 @@
+/*
+ * Created 2024.
+ * @author Evgeniy Isaenkov
+ * @github https://github.com/Udjin79/SRUtils
+ */
+
+package Examples.Postfunctions
+
+import com.onresolve.scriptrunner.runner.util.UserMessageUtil
+
+// This code will show a popup message to the user interface, indicating that the ticket
+// has been successfully processed and the DevOps team is informed.
+UserMessageUtil.success("Thank you for your ticket! The DevOps team has been notified via email and Slack.")
